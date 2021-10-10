2ba356761f68d7fb78e536e8760b211d19c681e3
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-09 23:09:18 -0400

    Added helper, and created notes.js route/index.js route

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,11 @@ const PORT = 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
 
 // GET route for homepage
 app.get('/', (req, res) => {
@@ -13,9 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // GET route for notes page
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
